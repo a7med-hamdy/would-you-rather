54fbb1930b3e9af14f8a7d86a80b68c53bf5e30b
a7med-hamdy <hamdiahmed906@gmail.com>
2021-09-17 17:26:14 +0200

    completed leaderboard

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { handleIntialData } from '../redux/actions/intial';
 import Home from '../views/Home';
 import NewQuestion from '../views/NewQuestion';
-
+import Leaderboard from '../views/Leaderboard';
 class App extends Component {
   componentDidMount(){
     this.props.dispatch(handleIntialData());
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NewQuestion />
+        {this.props.loading === true ? null :<Leaderboard />}
       </div>
     );
   }
---
+++ src/views/Leaderboard.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Leaderboard extends Component{
+    render(){
+        const {users} = this.props;
+        const arr = Object.keys(users);
+    
+        // sort users according to scores
+        arr.sort((a,b) => (users[b].questions.length + Object.keys(users[b].answers).length) - 
+        (users[a].questions.length + Object.keys(users[a].answers).length)
+        )
+        return(
+            <div className = 'leaderboard-area'>
+                <ul>
+                {arr.map((user) => 
+                   <li key = {users[user].name} >
+                       <h3>{users[user].name}</h3>
+                        <img
+                            src = {users[user].avatarURL} 
+                            className = 'avatar'
+                        />
+                        <p>Questions posted : {users[user].questions.length}</p>
+                        <p>Questions answered : {Object.keys(users[user].answers).length} </p>
+                        <h2>Score : {users[user].questions.length + Object.keys(users[user].answers).length}</h2>
+                   </li>
+                )}
+                </ul>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = ({users}) =>{
+    return{
+        users,
+    }
+}
+
+export default connect(mapStateToProps)(Leaderboard);
